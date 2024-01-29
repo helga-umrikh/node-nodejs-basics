aab628e36352d9e7f2b6b06d91883582809c1c18
helga-umrikh <zetaumr@gmail.com>
2024-01-30 00:58:08 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
-const copy = async () => {
-    // Write your code here 
+import fs from 'fs-extra';
+
+const copy = async (src, destination) => {
+
+    if (fs.existsSync(destination)) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs.copy(src, destination);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
-await copy();
+const srcFolder = './src/fs/files';
+const destinationFolder = './src/fs/files_copy';
+
+await copy(srcFolder, destinationFolder);
