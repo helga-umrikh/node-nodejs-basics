a5cd650c97797af7e492a0b1c07ad12967f528d3
helga-umrikh <zetaumr@gmail.com>
2024-01-30 00:41:24 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.writeFile('./fresh.txt', 'I am fresh and young')
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
