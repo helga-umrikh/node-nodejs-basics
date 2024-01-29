965466992fb186af978d2394b693992d128734b8
helga-umrikh <zetaumr@gmail.com>
2024-01-30 01:09:47 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+
 const rename = async () => {
-    // Write your code here 
+    const pathToProperFileName = './src/fs/files/properFilename.md';
+    const pathTowrongFileName = './src/fs/files/wrongFilename.txt';
+
+    if ((fs.existsSync(pathToProperFileName)) && (!fs.existsSync(pathTowrongFileName))) {
+        throw new Error('FS operation failed');
+    }
+
+    fs.rename(pathTowrongFileName,pathToProperFileName, (err) => {
+        if (err) {
+            throw new Error(`FS operation failed`)
+        }
+    });
 };
 
 await rename();
