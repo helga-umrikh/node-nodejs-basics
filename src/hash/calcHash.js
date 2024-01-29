0448f7e02da9747bef2efa0cee90c9cd46eb8fd7
helga-umrikh <zetaumr@gmail.com>
2024-01-30 01:57:40 +0300

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,33 @@
-const calculateHash = async () => {
-    // Write your code here 
+import crypto  from 'crypto';
+import  fs  from 'fs';
+
+const calculateHash = async (alg, fp) => {
+    return new Promise((resolve, reject) => {
+        const hash = crypto.createHash(alg);
+        const stream = fs.createReadStream(fp);
+    
+        stream.on('data', (data) => {
+            hash.update(data);
+        });
+    
+        stream.on('end', () => {
+            const calculatedHash = hash.digest('hex');
+            resolve(calculatedHash);
+        });
+    
+        stream.on('error', (error) => {
+            reject(error);
+        });
+    });
 };
 
-await calculateHash();
+const filePath = './src/hash/files/fileToCalculateHashFor.txt';
+const algorithm = 'sha256';
+
+await calculateHash(algorithm, filePath)
+    .then((hash) => {
+        console.log(hash)
+    })
+    .catch((err) => {
+        throw new Error(err);
+    })
