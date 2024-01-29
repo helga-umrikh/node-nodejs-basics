aec44b91275a8ad182148d3f5844863688acc08d
helga-umrikh <zetaumr@gmail.com>
2024-01-30 01:19:57 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = './src/fs/files';
+
+    fs.readdir(folderPath, (err, files) => {
+        const arrListOfFiles = [];
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+
+        files.forEach((file) => {
+            arrListOfFiles.push(file);
+        });
+
+        console.log(arrListOfFiles)
+    })
+
 };
 
 await list();
