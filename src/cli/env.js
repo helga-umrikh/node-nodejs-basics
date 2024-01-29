4ffe9bebe1b2f24c7ffd0c1cd2cbbbe30753b3c4
helga-umrikh <zetaumr@gmail.com>
2024-01-30 01:27:55 +0300

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    
+    Object.keys(process.env).forEach((key) => {
+        if (key.startsWith(prefix)) {
+            const value = process.env[key];
+            const name = key.substring(prefix.length);
+            console.log(`RSS_${name}=${value}`);
+        }
+    });
 };
 
 parseEnv();
