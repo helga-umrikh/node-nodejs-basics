d8b77dd0b95d2c832693e50523052f3eca40cf84
helga-umrikh <zetaumr@gmail.com>
2024-01-30 01:24:46 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from 'fs'
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = './src/fs/files/fileToRead.txt';
+
+    if(!fs.existsSync(pathToFile)) {
+        throw new Error('FS operation failed');
+    }
+
+    fs.readFile(pathToFile, 'utf8', (err, data) => {
+        if (err) {
+            throw new Error('FS operation failed')
+        }
+
+        console.log(data)
+    })
 };
 
 await read();
